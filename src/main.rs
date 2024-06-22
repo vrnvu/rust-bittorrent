7450252334c1615603a701aa6ddb6865c979eb76
ad <hi@arnaudiaz.com>
2024-06-22 16:33:46 +0200

    read bitfield from peer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use anyhow::bail;
+
 mod http;
 mod torrent;
 
@@ -14,10 +16,18 @@ async fn main() -> anyhow::Result<()> {
         .get(0)
         .expect("expected one peer at least");
 
-    let peer_stream = torrent::HandshakeMessage::new(torrent.info_hash_bytes)
+    let mut peer_stream = torrent::HandshakeMessage::new(torrent.info_hash_bytes)
         .send(peer)
         .await?;
     dbg!(&peer_stream);
+
+    if let torrent::PeerMessage::Bitfield(payload) =
+        torrent::PeerMessage::receive(&mut peer_stream.stream).await?
+    {
+        dbg!(&payload);
+    } else {
+        bail!("expected bitfield as first receive message from peer")
+    }
 
     Ok(())
 }
